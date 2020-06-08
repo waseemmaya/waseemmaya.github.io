96899c8ef25b44cb9a6a97791dcf1cbd7fac00bc
Waseem Maya <waseemmayaa@gmail.com>
2020-06-08 11:43:50 +0500

    first build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("Waseem");
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>Waseem Maya</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
